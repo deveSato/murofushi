3c30e3650a8f002aa2e0df1265ff21b0b337545d
turbo <turbosugar@gmail.com>
2020-05-22 08:25:29 +0900

    上へ戻るボタン追加

---
+++ script/scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener("click", e => {
+	const target = e.target;
+
+	// clickした要素がclass属性、js-smooth-scrollを含まない場合は処理を中断
+	if (!target.classList.contains("js-smooth-scroll")) {
+		console.log("fail!");
+		return;
+	}
+	e.preventDefault();
+	// const targetId = target.hash;
+	// document.querySelector(targetId).scrollIntoView({
+	// 	behavior: "smooth",
+	// 	block: "start"
+	// });
+	document.getElementsByClassName("heroSection")[0].scrollIntoView({
+		behavior: "smooth",
+		block: "start"
+	});
+});
