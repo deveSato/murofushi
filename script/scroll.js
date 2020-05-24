9df82f939cc03e38d6c56fe3ac1efe0285c66180
turbo <turbosugar@gmail.com>
2020-05-24 09:37:47 +0900

    smooth scroll テスト1

--- script/scroll.js
+++ script/scroll.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", e => {
 	const target = e.target;
 
-	// console.log(target.id)//DEBUG
+	console.log(target.id)//DEBUG
 	// clickした要素がtoTopを含まない場合は処理を中断
 	if (!target.id == "toTop") {
 		// console.log("fail!");////////DEBUG
@@ -13,8 +13,23 @@ document.addEventListener("click", e => {
 	// 	behavior: "smooth",
 	// 	block: "start"
 	// });
-	document.getElementById("topImage").scrollIntoView({
-		behavior: "smooth",
-		block: "start"
+	// document.getElementById("topImage").scrollIntoView({
+	// 	behavior: "smooth",
+	// 	block: "start"
+	// });
+
+	// 上のコードが無効になってしまった。。。。↓が代替コード
+	const targetElement = document.querySelector("#topImage");
+	const rectTop = targetElement.getBoundingClientRect().top
+	const offsetTop = window.pageYOffset
+	// スクロール位置に持たせるバッファ
+	const buffer = 0//50
+	const top = rectTop + offsetTop - buffer
+	window.scrollTo({
+		top,
+		behavior: "smooth"
 	});
+
 });
+
+
